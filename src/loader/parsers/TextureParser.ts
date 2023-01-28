9ca6c122010788e30aa862f1ee48960b14904a7e
HypnosNova <601670314@qq.com>
2023-01-28 00:11:04 -0800

    Update TextureParser.ts

--- src/loader/parsers/TextureParser.ts
+++ src/loader/parsers/TextureParser.ts
@@ -1,8 +1,7 @@
 import Texture from "../../components/texture/Texture";
-import { ILoadItem, IParser } from "../IResourceItem";
-import { Loader } from "../Loader";
+import { IParser } from "../IResourceItem";
 
-export const TextureParser: IParser<Texture> = async (loader: Loader, resource: ILoadItem<any>, blob: Blob) => {
+export const TextureParser: IParser<Texture> = async (blob: Blob) => {
     const bitmap = await createImageBitmap(blob);
     return new Texture(bitmap.width, bitmap.height, bitmap);
 }
