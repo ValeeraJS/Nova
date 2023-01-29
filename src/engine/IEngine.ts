4cf20ca1a2aaacc6ef61f498f1d8d36ae0464036
HypnosNova <601670314@qq.com>
2023-01-29 01:26:40 -0800

    Update IEngine.ts

--- src/engine/IEngine.ts
+++ src/engine/IEngine.ts
@@ -1,38 +1,14 @@
-import { ColorGPU } from "@valeera/mathx"
-import { ColorFormatType } from "../utils/getColorGPU"
-
-export default interface IEngine {
-	context: GPUCanvasContext | WebGLRenderingContext | WebGL2RenderingContext;
-}
-
 export enum EngineEvents {
-	INITED = "inited",
 	LOOP_STARTED = "loop-started",
 	LOOP_ENDED = "loop-ended",
 }
 
 export interface EngineOptions {
-	width?: number;
-	height?: number;
-	resolution?: number;
-	autoResize?: boolean;
-	noDepthTexture?: boolean;
-	clearColor?: ColorFormatType;
+	container?: HTMLElement;
 	autoStart?: boolean;
-	renderToSwapChain?: boolean;
-	renderToTarget?: boolean;
 }
 
-const DEFAULT_ENGINE_OPTIONS: Required<EngineOptions> = {
+export const DEFAULT_ENGINE_OPTIONS: Required<EngineOptions> = {
 	autoStart: true,
-	width: window.innerWidth,
-	height: window.innerHeight,
-	resolution: window.devicePixelRatio,
-	autoResize: false,
-	noDepthTexture: false,
-	clearColor: new ColorGPU(0, 0, 0, 1),
-	renderToSwapChain: true,
-	renderToTarget: false
+	container: document.body
 }
-
-export { DEFAULT_ENGINE_OPTIONS }
