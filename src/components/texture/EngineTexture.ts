2dbbd0251dc1c5b0c3be26d6896c3fc9fb319ba6
HypnosNova <601670314@qq.com>
2023-01-29 01:24:39 -0800

    Update EngineTexture.ts

--- src/components/texture/EngineTexture.ts
+++ src/components/texture/EngineTexture.ts
@@ -5,16 +5,16 @@ export default class EngineTexture extends Component<GPUTexture> {
 	width: number;
 	height: number;
 
-	public constructor(engine: WebGPUEngine, name: string = "texture-gpu") {
-		super(name, engine.device.createTexture({
-			size: [engine.options.width, engine.options.height],
-			format: engine.preferredFormat,
-			usage: GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.COPY_DST,
-		}));
-		this.width = engine.options.width;
-		this.height = engine.options.height;
-		this.dirty = true;
-	}
+	// public constructor(engine: WebGPUEngine, name: string = "texture-gpu") {
+	// 	super(name, engine.device.createTexture({
+	// 		size: [engine.options.width, engine.options.height],
+	// 		format: engine.preferredFormat,
+	// 		usage: GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.COPY_DST,
+	// 	}));
+	// 	this.width = engine.options.width;
+	// 	this.height = engine.options.height;
+	// 	this.dirty = true;
+	// }
 
 	public destroy() {
 		this.data = undefined;
