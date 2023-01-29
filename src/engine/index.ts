324f977fdca150b10229e191e1710eb5b6a955a8
HypnosNova <601670314@qq.com>
2023-01-29 01:26:58 -0800

    Update index.ts

--- src/engine/index.ts
+++ src/engine/index.ts
@@ -1,5 +1,3 @@
-export { default as WebGPUEngine } from "./WebGPUEngine";
-export { default as WebGLEngine } from "./WebGLEngine";
-export { EngineEvents } from "./IEngine";
-export { default as IEngine } from "./IEngine";
-export { EngineTaskChunk } from "./TaskChunk";
+export * from "./IEngine";
+export * from "./TaskChunk";
+export * from "./Engine";
