d5fd1ef6096879462e65743289a7c51a3ae40bc6
HypnosNova <601670314@qq.com>
2023-01-29 23:53:40 -0800

    Update WebGPURenderSystem.ts

--- src/systems/webgpu/WebGPURenderSystem.ts
+++ src/systems/webgpu/WebGPURenderSystem.ts
@@ -1,7 +1,7 @@
 import type { IWorld } from "@valeera/x";
 import { GPURendererContext, IWebGPURenderer } from "./IWebGPURenderer";
 import { RenderSystemInCanvas } from "../RenderSystem";
-import { IRenderSystemInCanvasOptions } from "../IRenderSystem";
+import { IRenderSystemWebGPUOptions } from "../IRenderSystem";
 import { IEntity } from "@valeera/x";
 import { RENDERABLE } from "../../components/constants";
 
@@ -36,13 +36,15 @@ export class WebGPURenderSystem extends RenderSystemInCanvas {
 	public currentCommandEncoder: GPUCommandEncoder;
 	public swapChainTexture: GPUTexture;
 	public targetTexture: GPUTexture;
+	public msaaTexture: GPUTexture;
 	private renderPassDescriptor: GPURenderPassDescriptor;
 
-	constructor(name: string = "WebGPU Render System", options: IRenderSystemInCanvasOptions = {}) {
+	constructor(name: string = "WebGPU Render System", options: IRenderSystemWebGPUOptions = {}) {
 		super(name, options);
 		WebGPURenderSystem.detect(this.canvas).then((data) => {
 			this.context = data as any;
 			this.context.preferredFormat = navigator.gpu.getPreferredCanvasFormat();
+			this.setMSAA(options.multisample ?? false);
 			this.setRenderPassDescripter();
 			data.gpu.configure({
 				device: data.device,
@@ -50,8 +52,47 @@ export class WebGPURenderSystem extends RenderSystemInCanvas {
 				format: this.context.preferredFormat,
 				alphaMode: "premultiplied"
 			});
+			this.targetTexture = this.context.device.createTexture({
+				size: [this.canvas.width, this.canvas.height],
+				format: this.context.preferredFormat,
+				usage: GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.COPY_SRC
+			});
+			if (options.multisample?.count > 1) {
+				this.msaaTexture = this.context.device.createTexture({
+					size: [this.canvas.width, this.canvas.height],
+					format: this.context.preferredFormat,
+					sampleCount: this.context.multisample ? (this.context.multisample.count ?? 1) : 1,
+					usage: GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.COPY_SRC
+				});
+			}
 			this.inited = true;
 		});
+	}
+
+	setMSAA(data: boolean | GPUMultisampleState) {
+		this.endTaskQueue.push(() => {
+			if (typeof data === 'boolean') {
+				if (data) {
+					this.context.multisample = {
+						count: 4
+					};
+				} else {
+					delete this.context.multisample;
+				}
+			} else {
+				if (data.count === 1) {
+					delete this.context.multisample;
+				} else {
+					this.context.multisample = data;
+				}
+			}
+
+			this.setRenderPassDescripter();
+			for (const renderer of this.rendererMap) {
+				renderer[1].clearCache();
+			}
+		});
+		return this;
 	}
 
 	public resize(width: number, height: number, resolution: number = this.resolution): this {
@@ -61,6 +102,10 @@ export class WebGPURenderSystem extends RenderSystemInCanvas {
 
 			if (this.targetTexture) {
 				this.targetTexture.destroy();
+			}
+			if (this.msaaTexture) {
+				this.msaaTexture.destroy();
+				this.msaaTexture = undefined;
 			}
 			this.targetTexture = this.context.device.createTexture({
 				size: [this.canvas.width, this.canvas.height],
@@ -102,14 +147,32 @@ export class WebGPURenderSystem extends RenderSystemInCanvas {
 	private loopStart() {
 		this.currentCommandEncoder = this.context.device.createCommandEncoder();
 		this.swapChainTexture = this.context.gpu.getCurrentTexture();
-		this.renderPassDescriptor.colorAttachments[0].view = this.swapChainTexture.createView();
+		if (this.context.multisample?.count > 1) {
+			if (!this.msaaTexture) {
+				this.msaaTexture = this.context.device.createTexture({
+					size: [this.canvas.width, this.canvas.height],
+					format: this.context.preferredFormat,
+					sampleCount: this.context.multisample ? (this.context.multisample.count ?? 1) : 1,
+					usage: GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.COPY_SRC
+				});
+			}
+			this.renderPassDescriptor.colorAttachments[0].view = this.msaaTexture.createView();
+			this.renderPassDescriptor.colorAttachments[0].resolveTarget = this.swapChainTexture.createView();
+		} else {
+			this.renderPassDescriptor.colorAttachments[0].view = this.swapChainTexture.createView();
+		}
 		this.context.passEncoder = this.currentCommandEncoder.beginRenderPass(this.renderPassDescriptor);
 	}
 
 	private loopEnd() {
 		this.context.passEncoder.end();
 		this.context.device.queue.submit([this.currentCommandEncoder.finish()]);
+		while(this.endTaskQueue.length) {
+			this.endTaskQueue.shift()();
+		}
 	}
+
+	private endTaskQueue: Function[] = [];
 
 	#depthTexture: GPUTexture | undefined;
 	private setRenderPassDescripter() {
@@ -130,6 +193,7 @@ export class WebGPURenderSystem extends RenderSystemInCanvas {
 			this.#depthTexture = this.context.device.createTexture({
 				size: { width: this.canvas.width, height: this.canvas.height, depthOrArrayLayers: 1 },
 				format: "depth24plus",
+				sampleCount: this.context.multisample ? (this.context.multisample.count ?? 1) : 1,
 				usage: GPUTextureUsage.RENDER_ATTACHMENT
 			});
 			renderPassDescriptor.depthStencilAttachment = {
