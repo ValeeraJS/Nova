d4208f3deb6d4f65fa74ebba373c4954704a33cb
hypnosnova <601670314@qq.com>
2023-03-05 20:03:47 +0800

    fix demo

--- build/Nova.module.js
+++ build/Nova.module.js
@@ -10420,37 +10420,23 @@ const createCamera3 = (projection, name = "camera", world) => {
     return entity;
 };
 
-const fs = `
+const vs = `
 struct Uniforms {
 	modelViewProjectionMatrix : mat4x4<f32>
 };
 @binding(0) @group(0) var<uniform> uniforms : Uniforms;
 
 struct VertexOutput {
-	@builtin(position) Position : vec4<f32>
-};
-
-fn mapRange(
-	value: f32,
-	range1: vec2<f32>,
-	range2: vec2<f32>,
-) -> f32 {
-	var d1: f32 = range1.y - range1.x;
-	var d2: f32 = range2.y - range2.x;
-
-	return (value - d1 * 0.5) / d2 / d1;
+	@builtin(position) position : vec4<f32>
 };
 
 @vertex fn main(@location(0) position : vec3<f32>) -> VertexOutput {
-	var output : VertexOutput;
-	output.Position = uniforms.modelViewProjectionMatrix * vec4<f32>(position, 1.0);
-	if (output.Position.w == 1.0) {
-		output.Position.z = mapRange(output.Position.z, vec2<f32>(-1.0, 1.0), vec2<f32>(1.0, 0.0));
-	}
-	return output;
+	var out: VertexOutput;
+	out.position = uniforms.modelViewProjectionMatrix * vec4<f32>(position, 1.0);
+	return out;
 }
 `;
-const vs = `
+const fs = `
 @fragment fn main() -> @location(0) vec4<f32> {
 	return vec4<f32>(1., 1., 1., 1.0);
 }
