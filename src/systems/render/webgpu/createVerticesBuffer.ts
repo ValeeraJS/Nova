6839bfe4680fc14bf8de96349eb092b0d12ee71b
大聪明 <601670314@qq.com>
2023-07-10 00:59:37 -0700

    Update createVerticesBuffer.ts

--- src/systems/render/webgpu/createVerticesBuffer.ts
+++ src/systems/render/webgpu/createVerticesBuffer.ts
@@ -1,13 +1,14 @@
 let descriptor: GPUBufferDescriptor = {
-    size: 0,
-    usage: GPUBufferUsage.VERTEX,
-    mappedAtCreation: true
+	size: 0,
+	usage: GPUBufferUsage.VERTEX,
+	mappedAtCreation: true
 };
 
-export default (device: GPUDevice, data: Float32Array) => {
-    descriptor.size = data.byteLength;
-    let buffer = device.createBuffer(descriptor);
-    new Float32Array(buffer.getMappedRange()).set(data);
-    buffer.unmap();
-    return buffer;
+export default (device: GPUDevice, data: Float32Array, usage: number = GPUBufferUsage.VERTEX) => {
+	descriptor.size = data.byteLength;
+	descriptor.usage = usage;
+	let buffer = device.createBuffer(descriptor);
+	new Float32Array(buffer.getMappedRange()).set(data);
+	buffer.unmap();
+	return buffer;
 }
