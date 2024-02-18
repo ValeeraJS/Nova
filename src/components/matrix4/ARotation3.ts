600570124f5e443352c4d668e5e3af548575424d
大聪明 <601670314@qq.com>
2024-02-17 18:32:05 -0800

    Update ARotation3.ts

--- src/components/matrix4/ARotation3.ts
+++ src/components/matrix4/ARotation3.ts
@@ -4,9 +4,9 @@ import Matrix4Component from "./Matrix4Component";
 
 export default abstract class ARotation3 extends Matrix4Component {
     constructor(data = Matrix4.create()) {
-        super(ROTATION_3D, data, [{
+        super(data, [{
 			label: ROTATION_3D,
 			unique: true
-		}]);
+		}], ROTATION_3D);
     }
 }
