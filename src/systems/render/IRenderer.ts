1bcef999e9a5abed4d874c68a0b53751196b5253
大聪明 <601670314@qq.com>
2024-02-17 18:43:04 -0800

    Update IRenderer.ts

--- src/systems/render/IRenderer.ts
+++ src/systems/render/IRenderer.ts
@@ -1,6 +1,6 @@
-import type { IEntity } from "@valeera/x";
+import type { Entity } from "@valeera/x";
 
 export interface IRenderer {
     renderTypes: string | string[];
-    render(entity: IEntity, ...args: any[]): any; // 处理渲染逻辑
+    render(entity: Entity, ...args: any[]): any; // 处理渲染逻辑
 }
