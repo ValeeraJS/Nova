63c441cc257b71931c1f371e25768e7e554355b1
大聪明 <601670314@qq.com>
2024-02-17 18:40:19 -0800

    Update createMesh2.ts

--- src/factory/entities/createMesh2.ts
+++ src/factory/entities/createMesh2.ts
@@ -1,4 +1,4 @@
-import { IWorld } from "@valeera/x";
+import { World } from "@valeera/x";
 import { MESH2 } from "../../components/constants";
 import { Object2 } from "../../entities/Object2";
 import { Geometry } from "../../systems/render";
@@ -6,7 +6,7 @@ import { DEFAULT_MATERIAL3 } from "../../systems/render/webgpu/material/defaultM
 import { IMaterial } from "../../systems/render/IMatrial";
 import { Mesh2 } from "../../systems/render/Mesh2";
 
-export const  createMesh2 = (geometry: Geometry, material: IMaterial = DEFAULT_MATERIAL3, name = MESH2, world?: IWorld): Object2 => {
+export const createMesh2 = (geometry: Geometry, material: IMaterial = DEFAULT_MATERIAL3, name = MESH2, world?: World): Object2 => {
 	const entity = new Object2(name);
 	entity.addComponent(new Mesh2(geometry, material));
 
