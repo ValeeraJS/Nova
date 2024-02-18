d2ce6ca91c1e2bc712f54dbde49ee5f79b1257e8
大聪明 <601670314@qq.com>
2024-02-17 18:35:41 -0800

    Update Object2.ts

--- src/entities/Object2.ts
+++ src/entities/Object2.ts
@@ -1,5 +1,5 @@
 import { Matrix3 } from "@valeera/mathx";
-import { Entity, IEntity } from "@valeera/x";
+import { Entity } from "@valeera/x";
 import { MODEL_2D, MODEL_3D, WORLD_MATRIX3 } from "../components/constants";
 import EuclidPosition2 from "../components/matrix3/EuclidPosition2";
 import AngleRotation2 from "../components/matrix3/AngleRotation2";
@@ -7,7 +7,7 @@ import Matrix3Component from "../components/matrix3/Matrix3Component";
 import Vector2Scale2 from "../components/matrix3/Vector2Scale2";
 import { Anchor2 } from "../components";
 
-export interface IObject2 extends IEntity {
+export interface IObject2 extends Entity {
     anchor: Matrix3Component;
     position: Matrix3Component;
     rotation: Matrix3Component;
@@ -23,7 +23,7 @@ export class Object2 extends Entity implements IObject2 {
     scaling: Matrix3Component;
     modelMatrix: Matrix3Component;
     worldMatrix: Matrix3Component;
-    constructor(name: string = "Object3") {
+    constructor(name: string = "Object2") {
         super(name);
         this.scaling = new Vector2Scale2();
         this.position = new EuclidPosition2();
