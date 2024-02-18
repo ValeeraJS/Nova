b8ffa4c18cd6128227b492fb9df8a4fcf95705cc
大聪明 <601670314@qq.com>
2024-02-17 18:39:49 -0800

    Update createCamera2.ts

--- src/factory/entities/createCamera2.ts
+++ src/factory/entities/createCamera2.ts
@@ -1,8 +1,8 @@
-import { IWorld } from "@valeera/x";
+import { World } from "@valeera/x";
 import { AProjection2 } from "../../components/matrix3";
 import { Camera2 } from "../../entities/Camera2";
 
-export const createCamera2 = (projection: AProjection2, name = "camera", world?: IWorld): Camera2 => {
+export const createCamera2 = (projection: AProjection2, name = "camera", world?: World): Camera2 => {
 	const entity = new Camera2(name, projection);
 
 	if (world) {
