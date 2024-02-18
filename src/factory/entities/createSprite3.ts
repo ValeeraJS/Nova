41acef58ae7e29153b3d79695ffd2b9ebaf0b072
大聪明 <601670314@qq.com>
2024-02-17 18:41:18 -0800

    Update createSprite3.ts

--- src/factory/entities/createSprite3.ts
+++ src/factory/entities/createSprite3.ts
@@ -1,13 +1,12 @@
-import { IWorld } from "@valeera/x";
+import { World } from "@valeera/x";
 import { SPRITE3 } from "../../components/constants";
 import { Object3 } from "../../entities/Object3";
 import { Sprite3 } from "../../systems/render/Sprite3";
 import { Texture } from "../../systems/render/texture/Texture";
 
-export const createSprite3 = (texture: Texture, name = SPRITE3 , world?: IWorld): Object3 => {
+export const createSprite3 = (texture: Texture, name = SPRITE3 , world?: World): Object3 => {
     const entity = new Object3(name);
     entity.addComponent(new Sprite3(texture));
-    
 
     if (world) {
         world.addEntity(entity);
