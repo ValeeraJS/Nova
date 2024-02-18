c3208134d0d64320d388243293de45ccf42d0cf6
大聪明 <601670314@qq.com>
2024-02-17 19:32:20 -0800

    Update TweenSystem.ts

--- src/systems/tween/TweenSystem.ts
+++ src/systems/tween/TweenSystem.ts
@@ -1,72 +1,76 @@
 import { Vector2, Vector3, Vector4 } from "@valeera/mathx";
-import { System, IEntity } from "@valeera/x";
+import { System, Entity } from "@valeera/x";
 import { Tween } from "./Tween";
 
 export class TweenSystem extends System {
     public constructor(name: string = "Tween System") {
-        super(name, (entity: IEntity): boolean => {
+        super((entity: Entity): boolean => {
             const component = entity.getComponent("tween") as Tween;
             if (!component) {
                 return false;
             }
             component.time = 0;
             return true;
-        });
+        }, () => { }, undefined, undefined, name);
     }
 
     public destroy(): this {
         throw new Error("Method not implemented.");
     }
 
-    public handle(entity: IEntity, _time: number, delta: number): this {
-        let tweenC = entity.getComponent("tween") as Tween;
-        if (tweenC.end) {
-            return this;
-        }
-        tweenC.time += delta;
-        if (tweenC.time > tweenC.duration) {
-            tweenC.loopTimes--;
-            if (tweenC.loopTimes >= 0) {
-                tweenC.time -= tweenC.duration;
-            } else {
-                tweenC.end = true;
-                tweenC.time = tweenC.duration;
+    public handle(entity: Entity, _time: number, delta: number): this {
+        let tweens = entity.getComponentsByClass(Tween) as Tween[];
+
+        for (let i = 0, len = tweens.length; i < len; i++) {
+            const tween = tweens[i];
+
+            if (tween.end) {
+                continue;
             }
-        }
-        let map = tweenC.data;
-        let from = tweenC.from;
-        let rate = tweenC.easing(tweenC.time / tweenC.duration);
-        if (from instanceof Float32Array) {
-            let data = map.get(' ') as any;
-            if (data.type === "vector2") {
-                Vector2.multiplyScalar(data.delta as Float32Array, rate, from as any);
-                Vector2.add(data.delta as Float32Array, data.origin as Float32Array, from as any);
-            } else if (data.type === "vector3") {
-                Vector3.multiplyScalar(data.delta as Float32Array, rate, from as any);
-                Vector3.add(data.delta as Float32Array, data.origin as Float32Array, from as any);
-            } else if (data.type === "vector4") {
-                Vector4.multiplyScalar(data.delta as Float32Array, rate, from as any);
-                Vector4.add(data.delta as Float32Array, data.origin as Float32Array, from as any);
+            tween.time += delta;
+            if (tween.time > tween.duration) {
+                tween.loopTimes--;
+                if (tween.loopTimes >= 0) {
+                    tween.time -= tween.duration;
+                } else {
+                    tween.end = true;
+                    tween.time = tween.duration;
+                }
+            }
+            let map = tween.data;
+            let from = tween.from;
+            let rate = tween.easing(tween.time / tween.duration);
+            if (from instanceof Float32Array) {
+                let data = map.get(' ') as any;
+                if (data.type === "vector2") {
+                    Vector2.multiplyScalar(data.delta as Float32Array, rate, from as any);
+                    Vector2.add(data.delta as Float32Array, data.origin as Float32Array, from as any);
+                } else if (data.type === "vector3") {
+                    Vector3.multiplyScalar(data.delta as Float32Array, rate, from as any);
+                    Vector3.add(data.delta as Float32Array, data.origin as Float32Array, from as any);
+                } else if (data.type === "vector4") {
+                    Vector4.multiplyScalar(data.delta as Float32Array, rate, from as any);
+                    Vector4.add(data.delta as Float32Array, data.origin as Float32Array, from as any);
+                }
+
+                return this;
             }
 
-            return this;
+            map.forEach((data, key) => {
+                if (data.type === "number") {
+                    from[key] = (data.origin as number) + (data.delta as number) * rate;
+                } else if (data.type === "vector2") {
+                    Vector2.multiplyScalar(data.delta as Float32Array, rate, from[key]);
+                    Vector2.add(data.delta as Float32Array, data.origin as Float32Array, from[key]);
+                } else if (data.type === "vector3") {
+                    Vector3.multiplyScalar(data.delta as Float32Array, rate, from[key]);
+                    Vector3.add(data.delta as Float32Array, data.origin as Float32Array, from[key]);
+                } else if (data.type === "vector4") {
+                    Vector4.multiplyScalar(data.delta as Float32Array, rate, from[key]);
+                    Vector4.add(data.delta as Float32Array, data.origin as Float32Array, from[key]);
+                }
+            });
         }
-
-        map.forEach((data, key) => {
-            if (data.type === "number") {
-                from[key] = (data.origin as number) + (data.delta as number) * rate;
-            } else if (data.type === "vector2") {
-                Vector2.multiplyScalar(data.delta as Float32Array, rate, from[key]);
-                Vector2.add(data.delta as Float32Array, data.origin as Float32Array, from[key]);
-            } else if (data.type === "vector3") {
-                Vector3.multiplyScalar(data.delta as Float32Array, rate, from[key]);
-                Vector3.add(data.delta as Float32Array, data.origin as Float32Array, from[key]);
-            } else if (data.type === "vector4") {
-                Vector4.multiplyScalar(data.delta as Float32Array, rate, from[key]);
-                Vector4.add(data.delta as Float32Array, data.origin as Float32Array, from[key]);
-            }
-        });
-
         return this;
     }
 
